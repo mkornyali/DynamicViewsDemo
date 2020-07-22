bdca7c096ec639c83b1b237358ae3919ceb487b7
mohamed korany <mkorany@victorylink.com>
2020-07-22 17:29:46 +0200

    first commit

---
+++ DynamicViewsDemo/DynamicViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DynamicViewController.swift
+//  DynamicViewsDemo
+//
+//  Created by Mohamed Korany on 7/22/20.
+//  Copyright © 2020 Mohamed Korany. All rights reserved.
+//
+
+import UIKit
+
+class DynamicViewController: UIViewController {
+
+    var dynamicViews:[UIView]?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+ 
+
+}
---
+++ DynamicViewsDemo/TableViewsEnumCases.swift
@@ -0,0 +1,9 @@
+//
+//  TableViewsEnumCases.swift
+//  DynamicViewsDemo
+//
+//  Created by Mohamed Korany on 7/22/20.
+//  Copyright © 2020 Mohamed Korany. All rights reserved.
+//
+
+import Foundation
